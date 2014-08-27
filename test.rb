6937e6329de33d3a4c1547a1f06ed7ecf7fdc797
GabrielleVivier <vivier.gabrielle@gmail.com>
2014-08-27 19:12:19 +0200

    more regex

--- test.rb
+++ test.rb
@@ -38,6 +38,8 @@ articles_number = content.scan(/ARTICLE\s+[\d]+/)
 
 article_title = content.scan(/ARTICLE\s[\d]+\.+[^\n]+/)
 
+puts article_title
+
 articles_content = content.scan(/(^ARTICLE.*?(?=ARTICLE|TITRE|\z))/m)
 
 company_name = content[/(^.*?(?=société|Société))/m].strip.gsub(/\s{2}+/, "")
@@ -57,14 +59,58 @@ articles_content.flatten.each do |article|
   company_share_capital << article if article[/(capital social|capital initial)/] && article[/libéré/] && !article[/apport/]
 end
 
-puts company_share_capital
 
-company_form = content[/(SAS|SARL|SA|SCI|EURL|SASU|S.A.S|S.A.RL|S.A|S.C.I|E.U.R.L|S.A.S.U)/]
+company_form = content[/(SAS|SARL|SA|SCI|EURL|SASU|S\.A\.S|S\.A\.R\.L|S\.A|S\.C\.I|E\.U\.R\.L|S\.A\.S\.U)/]
 
 company_purpose = []
 articles_content.flatten.each do |article|
-  company_purpose << article if article[/objet social est/] || article[/a pour objet/]
+  company_purpose << article if article =~ /objet social est/ || article =~ /a pour objet/
 end
+
+company_directors = []
+article_title.flatten.each do |title|
+  if title =~ /président directeur général/i
+    company_directors << "Président Directeur Général"
+  elsif title =~ /président/i
+    company_directors << "Président"
+  elsif title =~ /gérant/i
+    company_directors << "Gérant"
+  elsif title =~ /directoire/i
+    company_directors << "Directoire"
+  end
+  company_directors << "Directeur Général Délégué" if title =~ /directeur général délégué/i
+  company_directors << "Directeur Général" if title =~ /directeur général/i
+end
+
+powers_article = []
+
+company_directors.each do |director|
+  articles_content.flatten.each do |article|
+    powers_article << article if article =~ /(#{director} ne peut|#{director} ne pourra|#{director} ne pourront|autorisation préalable|sans l'accord)/i
+  end
+end
+
+power_chunk = []
+company_directors.each do |director|
+  powers_article.flatten.each do |article|
+    power_chunk << article.scan(/(?<=\.)([^\.]*(?:#{director} ne peut|#{director} ne pourra|#{director} ne pourront|autorisation préalable|sans l'accord)[^\.]*)(?=\.)/i)
+  end
+end
+
+company_corporate_bodies = []
+article_title.flatten.each do |title|
+  if title =~ /(comité|commission|direction)/i
+    company_corporate_bodies << title[/(ARTICLE\s+[\d]+.)(.+)/, 2]
+  end
+end
+
+puts company_corporate_bodies
+
+
+
+
+
+
 
 
 
